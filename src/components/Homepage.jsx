1e4d50586589f48d1ee5187b308fb457067e3a6e
Chris9533 <christopherbrownhill1995@gmail.com>
2022-06-14 12:48:55 +0100

    can now filter by category

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,7 +2,7 @@
 const HomePage = () => {
 return (
     <section>
-        <img id="homepageimg" src="https://familyapp.com/wp-content/uploads/2021/07/group-chat-etiquette-10-do-and-don-ts.jpg" />
+        <img id="homepageimg" src="https://familyapp.com/wp-content/uploads/2021/07/group-chat-etiquette-10-do-and-don-ts.jpg" alt="people socialising"/>
         <p>Welcome to NC games, a place where you can find all the latest reviews of your favourite games. Let people know what you think of their review by giving it a vote or leave a comment with your thougts. Happy reviewing!  </p>
     </section>
 )
--- src/utils/api.js
+++ src/utils/api.js
@@ -4,10 +4,18 @@ const gamesApi = axios.create({
     baseURL: `https://mysterious-dusk-03964.herokuapp.com/api`
 })
 
-export const getReviews = () => {
-    return gamesApi.get(`/reviews`)
+export const getReviews = (category) => {
+    return gamesApi.get(`/reviews`, { params : { category }})
     .then((res) => {
         return res.data.reviews;
+    })
+    
+}
+
+export const getCategories = () => {
+    return gamesApi.get(`/categories`)
+    .then((res) => {
+        return res.data.categories;
     })
     
 }
